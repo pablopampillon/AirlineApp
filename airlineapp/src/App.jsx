b5486995145b8d408ffaebdc0a6f0a2af9e09ac3
Xuebo Zhu <xbzh1994@gmail.com>
2022-11-30 11:41:30 +0100

    Styling flights

--- airlineapp/src/App.jsx
+++ airlineapp/src/App.jsx
@@ -1,4 +1,7 @@
 import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import "./App.css";
 import ReservaVuelo from "./Components/ReservaVuelo/ReservaVuelo";
 import FlightsList from "./Components/FlightsList/FlightsList";
